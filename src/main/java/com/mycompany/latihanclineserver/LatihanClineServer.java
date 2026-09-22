0f2d2116c5c4cb3aa460194c23be16802c81687a
Fahry <weaknestoneday@gmail.com>
2026-09-22 21:12:50 +0700

    Praktikum 1 - proyek Java pertama

---
+++ src/main/java/com/mycompany/latihanclineserver/LatihanClineServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanclineserver;
+
+/**
+ *
+ * @author INFINIX
+ */
+public class LatihanClineServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/pbi/tk/HelloKlienServer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbi.tk;
+
+public class HelloKlienServer {
+
+    public static void main(String[] args) {
+        System.out.println("==============================");
+        System.out.println("Nama  : [Nama Lengkap Anda]");
+        System.out.println("NIM   : [NIM Anda]");
+        System.out.println("Kelas : [Kelas Anda]");
+        System.out.println("==============================");
+        System.out.println("Aplikasi klien siap dijalankan");
+    }
+}
